0d0aa976ecb4ab16bbd51e9d8ab48befe214934d
ek2213 <ek2213@ic.ac.uk>
2015-10-23 23:53:31 +0100

    removed unused files

--- PlugBoard.cpp
+++
@@ -1,9 +0,0 @@
-#include "PlugBoard.hpp"
-
-PlugBoard::PlugBoard(int* config):AbstractGear(config) {
-	_config = config;
-}
-
-int PlugBoard::map(int letter) {
-	return 0;
-}
--- Util.hpp
+++
@@ -1,8 +0,0 @@
-#include "Rotor.hpp"
-#include "PlugBoard.hpp"
-#define ALPHA 26;
-
-class Util {
-    public:
-        static int* setConfig(char*);
-};
